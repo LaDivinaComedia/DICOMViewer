5e7777b0a19e3314ddfc2d30e7cfc95de00a4dd8
Vladyslav Vasyliev <v.vasyliev@innopolis.ru>
2016-09-21 12:42:14 +0300

    Commit with dummy test that is used to add a structure of the project's tests

---
+++ app/src/test/java/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileChooserTest.java
@@ -0,0 +1,15 @@
+package be.ac.ulb.lisa.idot.android.dicomviewer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Vladyslav Vasyliev
+ *         Created on 21.09.2016
+ */
+public class DICOMFileChooserTest {
+    @Test
+    public void testOnCreate() throws Exception {
+    }
+}
