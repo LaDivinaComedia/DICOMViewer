fab2975c564608bed8e8e8bff43c9e7ba855947a
Vladyslav Vasyliev <v.vasyliev@innopolis.ru>
2016-11-02 23:26:33 +0300

    Implemented reading of the Presentation State Annotations. Specifically: reading of the Graphic Annotation Sequences and its nested elements.

--- app/src/main/java/be/ac/ulb/lisa/idot/dicom/data/DICOMBody.java
+++ app/src/main/java/be/ac/ulb/lisa/idot/dicom/data/DICOMBody.java
@@ -1,45 +1,5 @@
-/*
- *
- * Copyright (C) 2011 Pierre Malarme
- *
- * Authors: Pierre Malarme <pmalarme at ulb.ac.be>
- *
- * Institution: Laboratory of Image Synthesis and Analysis (LISA)
- *              Faculty of Applied Science
- *              Universite Libre de Bruxelles (U.L.B.)
- *
- * Website: http://lisa.ulb.ac.be
- *
- * This file <DICOMBody.java> is part of Droid Dicom Viewer.
- *
- * Droid Dicom Viewer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Droid Dicom Viewer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Droid Dicom Viewer. If not, see <http://www.gnu.org/licenses/>.
- *
- * Released date: 17-02-2011
- *
- * Version: 1.0
- *
- */
-
 package be.ac.ulb.lisa.idot.dicom.data;
 
-/**
- * DICOM file meta informations.
- * 
- * @author Pierre Malarme
- * @version 1.0
- *
- */
 public class DICOMBody {
 	
 	// ---------------------------------------------------------------
@@ -449,7 +409,7 @@ public class DICOMBody {
 	}
 	
 	/**
-	 * @param mReferingPhysicianName the mReferingPhysicianName to set
+	 * @param mReferringPhysicianName the mReferingPhysicianName to set
 	 */
 	public void setReferringPhysicianName(String mReferringPhysicianName) {
 		this.mReferringPhysicianName = mReferringPhysicianName;
@@ -491,7 +451,7 @@ public class DICOMBody {
 	}
 	
 	/**
-	 * @param mPatientID the mPatientID to set
+	 * @param mPatientId the mPatientID to set
 	 */
 	public void setPatientId(String mPatientId) {
 		this.mPatientId = mPatientId;
@@ -533,7 +493,7 @@ public class DICOMBody {
 	}
 	
 	/**
-	 * @param mSamplePerPixel the mSamplePerPixel to set
+	 * @param mSamplesPerPixel the mSamplePerPixel to set
 	 */
 	public void setSamplesPerPixel(int mSamplesPerPixel) {
 		this.mSamplesPerPixel = mSamplesPerPixel;
---
+++ app/src/main/java/be/ac/ulb/lisa/idot/dicom/data/DICOMGraphicObject.java
@@ -0,0 +1,70 @@
+package be.ac.ulb.lisa.idot.dicom.data;
+
+import android.graphics.PointF;
+
+import be.ac.ulb.lisa.idot.dicom.data.DICOMAnnotation;
+
+import java.util.*;
+
+/**
+ * Created by vlad on 02.11.2016.
+ */
+public class DICOMGraphicObject {
+    public interface GraphicTypes {
+        String POINT = "POINT";
+        String POLYLINE = "POLYLINE";
+        String INTERPOLATED = "INTERPOLATED";
+        String CIRCLE = "CIRCLE";
+        String ELLIPSE = "ELLIPSE";
+
+        Set<String> TYPES = new HashSet<>(Arrays.asList(POINT, POLYLINE, INTERPOLATED, CIRCLE, ELLIPSE));
+    }
+
+    // graphic part of the annotation
+    private int mNumberOfGraphicPoints;
+    private List<PointF> mPoints;
+    private String mGraphicType;
+    private boolean mGraphicFilled;
+
+    public int getNumberOfGraphicPoints() {
+        return mNumberOfGraphicPoints;
+    }
+
+    public void setNumberOfGraphicPoints(int numberOfGraphicPoints) {
+        mNumberOfGraphicPoints = numberOfGraphicPoints;
+    }
+
+    public List<PointF> getPoints() {
+        return mPoints;
+    }
+
+    public void setPoints(float[] points) {
+        if (mPoints == null)
+            mPoints = new ArrayList<>();
+        else
+            mPoints.clear();
+        for (int i = 0; i < mNumberOfGraphicPoints * 2; i += 2)
+            mPoints.add(new PointF(points[i], points[i + 1]));
+    }
+
+    public void setPoints(List<PointF> points) {
+        mPoints = points;
+    }
+
+    public String getGraphicType() {
+        return mGraphicType;
+    }
+
+    public void setGraphicType(String type) {
+        mGraphicType = type;
+    }
+
+    public boolean isGraphicFilled() {
+        return mGraphicFilled;
+    }
+
+    public void setGraphicFilled(boolean graphicFilled) {
+        mGraphicFilled = graphicFilled;
+    }
+
+}
