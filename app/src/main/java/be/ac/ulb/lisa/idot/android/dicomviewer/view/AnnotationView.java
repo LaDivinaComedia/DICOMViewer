dc69d85b64228df3573e18dd454c4b1a337e5eee
iggytoto <iggytoto@gmail.com>
2016-11-16 18:25:26 +0300

    Vitya hacks

--- app/src/main/java/be/ac/ulb/lisa/idot/android/dicomviewer/view/AnnotationView.java
+++ app/src/main/java/be/ac/ulb/lisa/idot/android/dicomviewer/view/AnnotationView.java
@@ -200,6 +200,7 @@ public class AnnotationView extends ToolView implements View.OnTouchListener {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawPath(mCurrentPath.mPath, mPaint);
+        int count = 0;
         if (this.mCustomPaths.size() > 0) {
             for (int i = 0; i < this.mCustomPaths.size(); i++) {
                 canvas.drawPath(this.mCustomPaths.get(i).mPath, mPaints.get(i));
@@ -210,10 +211,11 @@ public class AnnotationView extends ToolView implements View.OnTouchListener {
                 DICOMAnnotation ann = mPresentationState.getAnnotations().get(i);
                 for (int j = 0; j < ann.getTextObjects().size(); j++) {
                     DICOMTextObject dtext = ann.getTextObjects().get(j);
-                    canvas.drawCircle(this.mLeftCorner[0] + dtext.getTextAnchor().x, this.mLeftCorner[1] + dtext.getTextAnchor().y, RADIUS * mScaleFactor / 2, mPaintsText.get(i + j));
+                    canvas.drawCircle(this.mLeftCorner[0] + dtext.getTextAnchor().x, this.mLeftCorner[1] + dtext.getTextAnchor().y, RADIUS * mScaleFactor / 2, mPaintsText.get(count));
                     canvas.drawText(
                             dtext.getText().substring(0, dtext.getText().length() >= 20 ? 20 : dtext.getText().length()),
-                            this.mLeftCorner[0] + dtext.getTextAnchor().x + RADIUS * mScaleFactor / 2, this.mLeftCorner[1] + dtext.getTextAnchor().y + RADIUS * mScaleFactor / 2, mPaintsText.get(i + j));
+                            this.mLeftCorner[0] + dtext.getTextAnchor().x + RADIUS * mScaleFactor / 2, this.mLeftCorner[1] + dtext.getTextAnchor().y + RADIUS * mScaleFactor / 2, mPaintsText.get(count));
+                    count++;
                 }
             }
         }
